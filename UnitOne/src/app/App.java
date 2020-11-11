15eb9857f25aa445a18b85713e83fcba50c8c28f
brandongasser <bgasser12@gmail.com>
2020-11-11 16:57:39 +0000

    completed bank

--- UnitOne/src/app/App.java
+++ UnitOne/src/app/App.java
@@ -1,138 +1,160 @@
 package app;
 
-import java.util.Random;
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
-        Random random = new Random();
+        SecureRandom random = new SecureRandom();
+        ArrayList<Account> accounts = new ArrayList<Account>();
+        Account activeAccount = new Account(0, 0, null);
+        boolean exit = false;
+        boolean inMainMenu;
+        boolean inAccountMenu;
 
-        String black = "black";
-        String red = "red";
-        String green = "green";
+        while (!exit) {
 
-        int chips = 100;
-
-        String colors[] = { green, red, black, red, black, red, black, red, black, red, black, black, red, black, red,
-                black, red, black, red, red, black, red, black, red, black, red, black, red, black, black, red, black,
-                red, black, red, black, red };
-        String betLocations[] = new String[10];
-        int betAmounts[] = new int[10];
-        int betTotal = 0;
-        int numBets = 0;
-
-        boolean cashedOut = false;
-
-        System.out.println("you have 100 chips");
-
-        do {
-            for (int i = 0; i < 10; i++) {
-                numBets++;
-                System.out.print("inside or outside?: ");
-                String inOut = sc.nextLine();
-                switch (inOut) {
-                    case "inside":
-                        System.out.print("what number?: ");
-                        betLocations[i] = sc.nextLine();
-                        break;
-                    case "outside":
-                        System.out.print("even, odd, black, or red?: ");
-                        betLocations[i] = sc.nextLine();
-                        break;
-                    default:
-                        break;
-                }
-                System.out.print("how much would you like to bet?: ");
-                int betAmount = sc.nextInt();
-                sc.nextLine();
-                betTotal += betAmount;
-                if (betTotal > chips) {
-                    System.out.println("exceeded balance and forced to cash out with " + chips + " chips");
-                    cashedOut = true;
-                    break;
-                } else {
-                    betAmounts[i] = betAmount;
-                }
-                if (i != 9) {
-                    System.out.print(
-                            "would you like to place another bet? you have " + (10 - numBets) + " bets remaining: ");
-                    String betAgain = sc.nextLine();
-                    if (betAgain.equals("no")) {
-                        break;
-                    }
-                }
-            }
-            if (!cashedOut) {
-                System.out.println("spinning...");
-                int rolledNumber = random.nextInt(37);
-                String rolledColor = colors[rolledNumber];
-                boolean rolledEven = rolledNumber % 2 == 0;
-                System.out.println(rolledColor + " " + rolledNumber);
-                if (rolledNumber == 0) {
-                    System.out.println("rolled zero. all bets lose");
-                    for (int i = 0; i < numBets; i++) {
-                        chips -= betAmounts[i];
-                    }
-                } else {
-                    for (int i = 0; i < numBets; i++) {
-                        if (betLocations[i].equals("even")) {
-                            if (rolledEven) {
-                                System.out.println(
-                                        "your bet of $" + betAmounts[i] + " on even was won (+$" + betAmounts[i] + ")");
-                                chips += betAmounts[i];
-                            } else {
-                                System.out.println("your bet of $" + betAmounts[i] + " on even was lost (-$"
-                                        + betAmounts[i] + ")");
-                                chips -= betAmounts[i];
+            // main menu
+            inMainMenu = true;
+            while (inMainMenu) {
+                System.out.print("create account (create), log in (log in), or exit (exit): ");
+                String input = sc.nextLine().toLowerCase();
+                switch (input) {
+                    case "create":
+                        System.out.print("initial deposit amount: ");
+                        float deposit = sc.nextFloat();
+                        sc.nextLine();
+                        System.out.print("password: ");
+                        String password = sc.nextLine();
+                        int createAccountNumber;
+                        boolean match;
+                        do {
+                            createAccountNumber = random.nextInt(9000) + 1000;
+                            match = false;
+                            for (Account account : accounts) {
+                                if (account.getAccountNumber() == createAccountNumber) {
+                                    match = true;
+                                }
                             }
-                        } else if (betLocations[i].equals("odd")) {
-                            if (!rolledEven) {
-                                System.out.println(
-                                        "your bet of $" + betAmounts[i] + " on odd was won (+$" + betAmounts[i] + ")");
-                                chips += betAmounts[i];
-                            } else {
-                                System.out.println(
-                                        "your bet of $" + betAmounts[i] + " on odd was lost (-$" + betAmounts[i] + ")");
-                                chips -= betAmounts[i];
+                        } while (match);
+                        System.out.println("your account number is " + createAccountNumber);
+                        Account newAccount = new Account(deposit, createAccountNumber, password);
+                        accounts.add(newAccount);
+                        activeAccount = newAccount;
+                        inMainMenu = false;
+                        break;
+                    case "log in":
+                        System.out.print("account number: ");
+                        int accountNumber = sc.nextInt();
+                        sc.nextLine();
+                        boolean foundAccount = false;
+                        for (Account account : accounts) {
+                            if (account.getAccountNumber() == accountNumber) {
+                                foundAccount = true;
+                                activeAccount = account;
                             }
-                        } else if (betLocations[i].equals("red") || betLocations[i].equals("black")) {
-                            if (betLocations[i].equals(rolledColor)) {
-                                System.out.println("your bet of $" + betAmounts[i] + " on " + betLocations[i]
-                                        + " was won (+$" + betAmounts[i] + ")");
-                                chips += betAmounts[i];
-                            } else {
-                                System.out.println("your bet of $" + betAmounts[i] + " on " + betLocations[i]
-                                        + " was lost (-$" + betAmounts[i] + ")");
-                                chips -= betAmounts[i];
-                            }
-                        } else if (Integer.toString(rolledNumber).equals(betLocations[i])) {
-                            System.out.println("your bet of $" + betAmounts[i] + " on " + betLocations[i]
-                                    + " was won (+$" + (betAmounts[i] * 34) + ")");
-                            chips += betAmounts[i] * 34;
-                        } else {
-                            System.out.println("your bet of $" + betAmounts[i] + " on " + betLocations[i]
-                                    + " was lost (-$" + betAmounts[i] + ")");
-                            chips -= betAmounts[i];
                         }
-                    }
-                }
-                System.out.println("you have " + chips + " chips");
-                System.out.print("would you like to play another round?: ");
-                String playAgain = sc.nextLine();
-                switch (playAgain) {
-                    case "no":
-                        cashedOut = true;
+                        if (foundAccount) {
+                            System.out.print("password: ");
+                            if (activeAccount.checkPassword(sc.nextLine())) {
+                                inMainMenu = false;
+                            } else {
+                                System.out.println("password is not correct");
+                            }
+                        } else {
+                            System.out.println("could not find account");
+                        }
+                        break;
+                    case "exit":
+                        System.out.println("goodbye");
+                        inMainMenu = false;
+                        exit = true;
                         break;
                     default:
-                        numBets = 0;
-                        betTotal = 0;
+                        System.out.println("not a valid command");
                         break;
                 }
             }
-        } while (!cashedOut);
-        System.out.println("you cashed out with " + chips + " chips");
+
+            // account menu
+            if (!exit) {
+                inAccountMenu = true;
+                while (inAccountMenu) {
+                    System.out.print("check balance (check), deposit (deposit), withdraw (withdraw), transfer (transfer), change password (change password), log out (log out): ");
+                    String input = sc.nextLine().toLowerCase();
+                    switch (input) {
+                        case "check":
+                            System.out.println("$" + activeAccount.getBalance());
+                            break;
+                        case "deposit":
+                            System.out.print("how much would you like to deposit: ");
+                            activeAccount.deposit(sc.nextFloat());
+                            sc.nextLine();
+                            break;
+                        case "withdraw":
+                            System.out.print("how much would you like to withdraw: ");
+                            activeAccount.withdraw(sc.nextFloat());
+                            sc.nextLine();
+                            break;
+                        case "transfer":
+                            System.out.print("account number of other account: ");
+                            int targetAccountNumber = sc.nextInt();
+                            sc.nextLine();
+                            Account targetAccount = null;
+                            for (Account account : accounts) {
+                                if (account.getAccountNumber() == targetAccountNumber) {
+                                    targetAccount = account;
+                                }
+                            }
+                            if (targetAccount != null) {
+                                System.out.print("transfer amount: ");
+                                float transferAmount = sc.nextFloat();
+                                sc.nextLine();
+                                if (transferAmount > activeAccount.getBalance()) {
+                                    System.out.println("insufficient funds");
+                                } else {
+                                    activeAccount.withdraw(transferAmount);
+                                    targetAccount.deposit(transferAmount);
+                                }
+                            } else {
+                                System.out.println("account not found");
+                                System.out.println("transfer cancelled");
+                            }
+                            break;
+                        case "change password":
+                            System.out.print("enter old password: ");
+                            if (activeAccount.checkPassword(sc.nextLine())) {
+                                System.out.print("enter new password: ");
+                                String newPassword = sc.nextLine();
+                                System.out.print("confirm new password: ");
+                                if (sc.nextLine().equals(newPassword)) {
+                                    activeAccount.changePassword(newPassword);
+                                    System.out.println("password successfully changed");
+                                } else {
+                                    System.out.println("passwords don't match");
+                                    System.out.println("cancelling password change");
+                                }
+                            } else {
+                                System.out.println("password is incorrect");
+                                System.out.println("cancelling password change");
+                            }
+                            break;
+                        case "log out":
+                            System.out.println("logging out");
+                            inAccountMenu = false;
+                            break;
+                        default:
+                            System.out.println("not a valid command");
+                            break;
+                    }
+                }
+            }
+
+        }
+        sc.close();
 
     }
 
